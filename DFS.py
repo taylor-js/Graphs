a166e22fc1f859362e74a456bc4512c4573d5056
Taylor J Saintable <tsaintable@gmail.com>
2020-02-14 19:21:21 -0200

    Initial commit

---
+++ DFS.py
@@ -0,0 +1,18 @@
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    print(start)
+    for next in graph[start] - visited:
+        dfs(graph, next, visited)
+    return visited
+
+graph = {'A': set(['B', 'C']),
+         'B': set(['A', 'D', 'E']),
+         'C': set(['A', 'F']),
+         'D': set(['B']),
+         'E': set(['B', 'F']),
+         'F': set(['C', 'E'])}
+
+
+dfs(graph, 'A')
---
+++ Dijkstra.py
@@ -0,0 +1,31 @@
+from heapq import heappush, heappop
+
+def dijkstra(graph, source):
+    distances = {}
+    predecessors = {}
+    seen = {source: 0}
+    priority_queue = [(0, source)]
+
+    while priority_queue:
+        v_dist, V = heappop(priority_queue)
+        distances[V] = v_dist
+
+        for w in graph[V]:
+            vw_dist = distances[V] + 1
+            if w not in seen or vw_dist < seen[w]:
+                seen[w] = vw_dist
+                heappush(priority_queue, (vw_dist,w))
+                predecessors[w] = V
+    return distances#, predecessors
+    
+g = {'A': ['B', 'C'],
+     'B': ['C', 'D'],
+     'C': ['A', 'B', 'D', 'E', 'F'],
+     'D': ['B', 'C', 'E', 'F'],
+     'E': ['F', 'D'],
+     'F': ['C', 'D', 'E']}
+
+#print(g) # {'A': ['B', 'C'], 'B': ['C', 'D'], 'C': ['D'], 'D': ['C'], 'E': ['F'], 'F': ['C']}
+#print(dijkstra(g,'A'))# {'A': 0, 'B': 1, 'C': 1, 'D': 2}, {'B': 'A', 'C': 'A', 'D': 'B'}
+
+
