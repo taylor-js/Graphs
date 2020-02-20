98bd688b9a3276b137f68e08af4bb076c3d0a129
Taylor J Saintable <tsaintable@gmail.com>
2020-02-19 23:08:00 -0200

    Added new edges to graph

--- BellmanFord.py
+++ BellmanFord.py
@@ -59,8 +59,9 @@ g.addEdge(1, 3, 2)
 g.addEdge(1, 4, 2) 
 g.addEdge(3, 2, 5) 
 g.addEdge(3, 1, 1) 
-g.addEdge(4, 3, -3) 
+g.addEdge(4, 3, -3)
+g.addEdge(4, 2, 2) 
+g.addEdge(4, 1, -4)  
 # Print the solution 
 g.BellmanFord(0)
-
 # This code is contributed by Neelam Yadav 
